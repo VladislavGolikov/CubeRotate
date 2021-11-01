3d09c4df13f67dc33eb324fdf5b3434a7e8150c7
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-11-01 14:25:18 +0300

    добавил файл вебпак конфиг с минимальной пробной начальной конфигурацией

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+﻿const path=require('path');
+
+module.exports={
+  mode: 'development',
+  entry: './index.html',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
