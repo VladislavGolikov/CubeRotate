b47aa6a2c8b0ede3d998464893ab28a915484354
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-11-09 16:04:37 +0300

    переложил рабочие файлы по папкам: код в папку исходников, картинку в папку контекст. немного продолжил настраивать конфиг вебпака. добавил файл переменных окружения, для последующей установки модуля дотенв. гитигнор добавил переменные окружения и папку со сборками...

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-﻿const path=require('path');
+const path=require('path');
 
+//const NODE_ENV=process.env;
+//console.log(NODE_ENV)
 module.exports={
     mode: 'development',
+
+
+    context: path.resolve(__dirname, 'source-files'),
     entry: './perspectivo-interface.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: true ? path.resolve(__dirname, `destination/for-development`) : path.resolve(__dirname, 'destination/for-production'),
         filename: 'bundle.js',
         library: 'CubeRotate',
     },
-    watch: true,
+    watch: false,
+
     watchOptions: {
         aggregateTimeout: 100
     },
