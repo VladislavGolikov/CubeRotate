b346ce660fcfd86f71d5423b2145a42a59b1ca51
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-12-14 13:43:58 +0300

    конфиг изменен: путь сборки меняется в зависимости от переменной окружения

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,15 @@ const path=require('path');
 require('dotenv').config({path: __dirname + '/project.env'}); /* для использования переменных окружения с указанием файла */
 
 const {assembly}=process.env; /* определение переменных окружения */
-console.log(assembly)
 
 module.exports={
-    mode: 'development',
+    mode: assembly,
 
 
     context: path.resolve(__dirname, 'source-files'),
     entry: './perspectivo-interface.js',
     output: {
-        path: true ? path.resolve(__dirname, `destination/for-development`) : path.resolve(__dirname, 'destination/for-production'),
+        path: assembly==`development` ? path.resolve(__dirname, `destination/for-development`) : path.resolve(__dirname, 'destination/for-production'),
         filename: 'bundle.js',
         library: 'CubeRotate',
     },
