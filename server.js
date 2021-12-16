9e7eaa45defd601e287304b71f14f17b2798ab7d
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-12-16 05:54:45 +0300

    был глобально установлен экспресс, чтобы теперь всегда был доступен сервер для проб проектов. в текущем проекте добавлен server.js - начальный вариант, который пока просто запускается и слушает порт, но умеет вызывать экспресс, установленный глобально

---
+++ server.js
@@ -0,0 +1,41 @@
+
+const path=require('path');
+
+const {execSync}=require('child_process'); /* для запуска командной строки */
+const express=require(execSync(`npm root -g`, {encoding: 'utf8'}).trim()+'\\express'); /* вызов глобального модуля */
+
+/* для вызова модуля, установленного глобально, приходится ухищряться:
+запускать командную строку, чтобы считать путь к установке, тримить ее ответ
+ (там на конце перевод строки cmd возвращает) и еще экраноировать обратный слеш */
+
+
+
+
+//const colors=require('colors');
+
+const app=express();
+
+
+
+
+const port=3000;
+//const pathToStaticFiles='/files';
+
+
+//app.set('views', './template');
+//app.set('view engine', 'pug');
+/*
+app.get('/', function (req, res) {
+    console.log(`ко мне обратились`.blue);
+    res.render('netcoin_main',{mainname: 'это гет'});
+});
+
+*/
+
+
+//app.use(express.urlencoded({extended: false}));
+//app.use(express.json());
+//app.use(express.static(`${__dirname}${pathToStaticFiles}`));
+
+app.listen(port,()=>{console.log(`Это экспресс запущен в работу... Текущий порт ${port}`)});
+
