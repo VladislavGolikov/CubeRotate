3843ce1832a3ddefefda41a3cac02d1b8056ae1b
Vladislav Golikov <vladislav.v.golikov@gmail.com>
2021-12-20 15:42:19 +0300

    сброшены неправильные изменения, все готово для дальнейших экспериментов с вебпаком

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 const path=require('path');
+const colors=require('colors');
 
 const {execSync}=require('child_process'); /* для запуска командной строки */
 const express=require(execSync(`npm root -g`, {encoding: 'utf8'}).trim()+'\\express'); /* вызов глобального модуля */
@@ -8,34 +9,33 @@ const express=require(execSync(`npm root -g`, {encoding: 'utf8'}).trim()+'\\expr
 запускать командную строку, чтобы считать путь к установке, тримить ее ответ
  (там на конце перевод строки cmd возвращает) и еще экраноировать обратный слеш */
 
-
-
-
-const colors=require('colors');
-
+const port=4000;
+const pathToIndex=`${__dirname}/source-files/index.html`;
 const app=express();
 
 
 
 
-const port=3000;
 //const pathToStaticFiles='/files';
 
 
 //app.set('views', './template');
 //app.set('view engine', 'pug');
-/*
+
 app.get('/', function (req, res) {
-    console.log(`ко мне обратились`.blue);
-    res.render('netcoin_main',{mainname: 'это гет'});
+    console.log(`простой гет запрос...`.blue);
+
+    res.sendFile(pathToIndex);
+//    res.render('это гет');
+
 });
 
-*/
+
 
 
 //app.use(express.urlencoded({extended: false}));
 //app.use(express.json());
-//app.use(express.static(`${__dirname}${pathToStaticFiles}`));
+app.use(express.static(`${__dirname}/source-files`));
 
 app.listen(port,()=>{console.log(`Это экспресс запущен в работу...`.brightGreen+`Текущий порт ${port}`.bgYellow.black)});
 
